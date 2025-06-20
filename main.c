70171d1e69b88b07b7cbd98b9b8d60ae07852d0f
Gustans04 <129980714+Gustans04@users.noreply.github.com>
2025-06-19 23:49:54 -0300

    Rascunho da main.c
    Um rascunho de como eu imagino que a main deve ser estruturada. Tal rascunho foi feito antes da criação das funções.

--- main.c
+++ main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "partida.h"
 
 int main(void) {
+    int resume;
+    int escolha_menu;
+    int continuar = 1;
+    int aposta;
+    int voltar_menu;
+
+    inicia_jogo(&resume, &aposta);
+    
+    while (1){
+        chama_menu(&escolha_menu, resume);
+
+        if(escolha_menu == 2){ // usuário escolheu sair
+            fecha_jogo(aposta, resume);
+            exit(1);
+        }
+
+        while(continuar){
+            if(escolha_menu == 0) inicia_rodada(&aposta); // usuário escolheu novo jogo
+
+            turno_usuario(aposta, &voltar_menu);
+            if(voltar_menu == 1) { // usuário escolheu voltar ao menu
+                resume = 1;
+                break; 
+            }
+
+            turno_dealer(aposta);
+            fim_de_rodada(aposta, &continuar, &resume);
+        }
+    }
+    
+
 
   return 0;
 }
