f3145f9b690f533d7d960254f26fffe647a8f7ef
diogo-benchimol <146952584+diogo-benchimol@users.noreply.github.com>
2025-06-23 11:51:16 -0300

    Create interface.h

---
+++ cJSON/interface.h
@@ -0,0 +1,13 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+int menu_principal(int resume);
+int interface_rodada(int tipo_jogador, int aposta);
+int interface_fim(int resultado, int aposta);
+int define_aposta(int* aposta, int dinheiro_disponivel);
+void game_over();
+void imprime_cartas(int tipo_jogador);
+void imprime_status(int tipo_jogador, int dinheiro);
+void imprime_msg(const char* msg);
+
+#endif
