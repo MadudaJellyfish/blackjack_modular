bde842a30941fafe5b2acbcdab78ced66509d13c
Gustans04 <129980714+Gustans04@users.noreply.github.com>
2025-06-24 15:03:36 -0300

    Retirando funções que não são de interface

--- interface.h
+++ interface.h
@@ -1,14 +1,10 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-
-int menu_principal(int resume);
-int interface_rodada(int tipo_jogador, int aposta);
-int interface_fim(int resultado, int aposta);
+int menu_principal(int* escolha, int resume);
+int interface_rodada(int tipo_jogador, int aposta, int* jogada);
 int define_aposta(int* aposta, int dinheiro_disponivel);
-void game_over();
-void imprime_cartas(int tipo_jogador);
-void imprime_status(int tipo_jogador, int dinheiro);
-void imprime_msg(const char* msg);
+int interface_fim(int resultado, int aposta, int* deseja_continuar);
+int game_over(void);
 
 #endif
