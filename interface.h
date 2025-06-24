e6c29f69068ad7eaed702808c84fa9bbbc902452
Gustans04 <129980714+Gustans04@users.noreply.github.com>
2025-06-24 15:48:37 -0300

    Pequenas adaptações para integração

--- interface.h
+++ interface.h
@@ -6,5 +6,7 @@ int interface_rodada(int tipo_jogador, int aposta, int* jogada);
 int define_aposta(int* aposta, int dinheiro_disponivel);
 int interface_fim(int resultado, int aposta, int* deseja_continuar);
 int game_over(void);
+const char* naipe_to_string(int naipe);
+const char* valor_to_string(int valor);
 
 #endif
