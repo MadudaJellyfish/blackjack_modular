ef054548ccc131494cd38ca007fb1f74adeb337f
Gustans04 <129980714+Gustans04@users.noreply.github.com>
2025-06-25 11:50:15 -0300

    mudando imprime cartas para .h

--- interface.h
+++ interface.h
@@ -6,6 +6,7 @@ int interface_rodada(int tipo_jogador, int aposta, int* jogada);
 int define_aposta(int* aposta, int dinheiro_disponivel);
 int interface_fim(int resultado, int aposta, int* deseja_continuar);
 int game_over(void);
+void imprime_cartas(int tipo_jogador);
 const char* naipe_to_string(int naipe);
 const char* valor_to_string(int valor);
 
